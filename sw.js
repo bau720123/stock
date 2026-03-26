792235623fd7a07c8183a6d1cf532242c0d95458
TPECNS\colin.bau <6564546>
2026-03-26 16:54:23 +0800

    update

--- sw.js
+++ sw.js
@@ -45,12 +45,6 @@ self.addEventListener('push', event => {
 });
 
 // 使用者點擊通知時觸發
-// self.addEventListener('notificationclick', event => {
-//   event.notification.close();
-//   if (event.action === 'dismiss') return; // 忽略就直接關掉
-//   const url = event.notification.data?.url || '/stock/index.html';
-//   event.waitUntil(clients.openWindow(url));
-// });
 self.addEventListener('notificationclick', event => {
   event.notification.close();
   const action = event.action || '（點擊通知本身）';
