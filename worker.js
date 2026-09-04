7278f3a86a8aa74e0fe64fb9c7d65f84ac4e144b
TPECNS\colin.bau <6564546>
2026-09-04 10:40:02 +0800

    update

--- worker.js
+++ worker.js
@@ -183,7 +183,7 @@ export default {
     if (event.cron === "0 */1 * * *") {
       ctx.waitUntil(handleCron(env)); // 原本的每小時 Web Push 摘要
     }
-    if (event.cron === "*/2 0-6 * * 1-5") {
+    if (event.cron === "*/2 0-6 * * *") {
       ctx.waitUntil(handleHistoryBackground(env)); // 日盤期間每 2 分鐘記錄一次盤勢
     }
   }
